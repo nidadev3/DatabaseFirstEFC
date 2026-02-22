73c7ff4680745566d6604574581da76d7c8177d1
Nida Arshad <nidaarshadsocials120@gmail.com>
2026-02-21 21:15:59 -0800

    First Commit

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirstEFCore.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public string StudentGender { get; set; } = null!;
+
+    public int StudentAge { get; set; }
+}
