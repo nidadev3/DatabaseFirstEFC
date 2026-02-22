0c10313bd0ee54d0ae7b7c61770a6a6732ee1853
Nida Arshad <nidaarshadsocials120@gmail.com>
2026-02-21 22:00:33 -0800

    Completed Crud Application

--- obj/Debug/net8.0/DatabaseFirstEFCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/DatabaseFirstEFCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseFirstEFCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c7ff4680745566d6604574581da76d7c8177d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseFirstEFCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseFirstEFCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
